6b1d97268e8d9f87a964a60a4854ce100b645dd0
Keu Santos <modulodigital69@gmail.com>
2023-01-25 10:28:53 -0300

    criar endpoint GET movie por ID#3

--- src/controllers/movies/index.ts
+++ src/controllers/movies/index.ts
@@ -15,9 +15,25 @@ export const getMovies = (req, res) => {
   const movies = moviesRepository
     .find()
     .then((response) => {
-      return res.status(200).json(movies)
+      return res
+        .status(200)
+        .json(movies, { message: "Filme encontradas com sucesso." })
     })
     .catch((error) => {
       res.status(500).send(error)
+    })
+}
+
+export const getMovieId = (req, res) => {
+  const moviesIdRepository = getRepository(Movies)
+  const moviesId = moviesIdRepository
+    .find()
+    .then((response) => {
+      return res.status(200).json(moviesId)
+    })
+    .catch((error) => {
+      return res.status(401).json({
+        message: "Falha ao encontrar Filme.",
+      })
     })
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,9 @@
-import { getMovies, itsWorks } from "@controllers/movies"
+import { getMovieId, getMovies, itsWorks } from "@controllers/movies"
 import { getLogin } from "@controllers/user"
 
 export const defineRoutes = (app) => {
   app.get("/", itsWorks)
   app.get("/movies", getMovies)
+  app.get("/movies/:id", getMovieId)
   app.get("/login", getLogin)
-
-  app.post("/movies/:id")
 }
